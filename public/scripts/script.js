33c8fb1960def8f13ea7037a23fe8f86f461fde4
Gabriel Gonçalves Pereira <ggonpereira@gmail.com>
2020-11-23 00:34:25 -0300

    Projeto finalizado. Responsividade e JS adicionados.
    Amém, mais um projeto de muitos finalizado!

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,37 @@
-let hamburger = document.querySelector(".ham-icon");
-hamburger.addEventListener("click", toggleClass);
+// ** Play music area ** //
+const audio = new Audio("http://regradetres.tk/img/one_more_light.mp3"); // música está hospedada num outro projeto, mudarei em breve.
+const playMusicButton = document.querySelector(".music");
+playMusicButton.addEventListener("click", toggleMusicOnOff);
+
+function toggleMusicOnOff() {
+  if (playMusicButton.classList.contains("fa-play-circle")) {
+    audio.play();
+    playMusicButton.classList.remove("fa-play-circle");
+    playMusicButton.classList.add("fa-pause-circle");
+  } else {
+    audio.pause();
+    playMusicButton.classList.remove("fa-pause-circle");
+    playMusicButton.classList.add("fa-play-circle");
+  }
+}
+
+// ** Phrases area ** //
+const phraseArea = document.querySelector(".main-header-area > h1");
+phraseArea.addEventListener("click", togglePhrase);
+
+const phraseEN = "Who cares if one more light goes out? Well, I do.";
+const phrasePT =
+  "Quem se importa se mais uma luz se apagar? Bem, eu me importo.";
 
 function toggleClass() {
-  let menu = document.querySelector(".main-menu");
+  const menu = document.querySelector(".main-menu");
   menu.classList.toggle("toggle-cls");
+}
+
+function togglePhrase() {
+  if (phraseArea.innerHTML.includes(phraseEN)) {
+    phraseArea.innerHTML = phrasePT;
+  } else {
+    phraseArea.innerHTML = phraseEN;
+  }
 }
